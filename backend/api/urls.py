421f568e00725e4e0d997803d39f03515847cc78
akhilesh_verma <146834811+akhileshverma92@users.noreply.github.com>
2025-08-22 08:24:10 +0530

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import submit_form, view_submissions
 
 urlpatterns = [
-    path('submit/', submit_form),
+    path('submit/', submit_form, name='submit'),  # endpoint is /api/submit/
     path('view/', view_submissions),
 ]
+
